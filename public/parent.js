7fab59849c32f0cdc1153cdc7235289d253db3e5
Kim Dang <kimdangg@gmail.com>
2016-11-05 15:02:12 -0700

    update points

--- public/parent.js
+++ public/parent.js
@@ -1,14 +1,31 @@
+var credit = 250;
+
 function addCredit() {
   $.ajax({
     type: "put",
     data: {"add": true},
     url: "/credit",
     success: function(data) { console.log('points added!');}
+  });
+}
+
+function getCredit() {
+  $.ajax({
+    type: "get",
+    url: "/credit",
+    success: function(data) {
+      console.log('got credits!');
+      credit = data.credit;
+    }
  });
 }
 
 $(document).ready(function() {
+  getCredit();
+  document.getElementById("earned").innerHTML = credit;
   $(".target").on('click', function() {
     addCredit();
+    getCredit();
+    document.getElementById("earned").innerHTML = credit;
   });
 });
