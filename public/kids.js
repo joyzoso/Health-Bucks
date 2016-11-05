e981cd358433c49a87630dfbcf2ec59ae84dbe1c
Daniel Cudney <upnvapesd@gmail.com>
2016-11-05 14:44:57 -0700

    fixed

--- public/kids.js
+++ public/kids.js
@@ -1,7 +1,36 @@
+let user = {"parent":"","username":"","credit":0,"minecraftTime":{"available":0,"total":0}};
+
+
+function saveUser(user) {
+  $.ajax({
+    url: "/credit", type: 'PUT', data: user, success: function(res) { }
+  })
+}
+
+var mcBuyTime = function() {
+  if (user.credit >= 1) {
+    user.credit -= 1;
+    document.getElementById("earned").innerHTML = user.credit
+    saveUser({'checkoutCredit': 1})
+  }
+}
+var buyNike = function() {
+  if (user.credit >= 100) {
+    user.credit -= 100;
+    document.getElementById("earned").innerHTML = user.credit
+    saveUser({'checkoutCredit': 100})
+  }
+}
+
 $(document).ready(function() {
   $.ajax({
     url: "/credit", success: function(res) {
-      console.log(res)
+      user = res.user;
+      document.getElementById("earned").innerHTML = res.user.credit;
+
     }
   })
+
+
+
 })
