34422009801b639ece73a06bdb08f4d42c5abf7e
Daniel Cudney <upnvapesd@gmail.com>
2016-11-05 11:15:09 -0700

    MineCraft Route

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var mineCraftRoute = express.Router();
+var parser = require('body-parser');
 
+app.use(parser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+//Global data
+var bucksData;
+
 
 // Asynchronously read file contents, then call callbackFn
 function readFile(filename, defaultData, callbackFn) {
@@ -34,9 +40,58 @@ function initServer() {
   // When we start the server, we must load the stored data
   var defaultList = "[]";
   readFile("data.txt", defaultList, function(err, data) {
-    listings = JSON.parse(data);
+    bucksData = JSON.parse(data);
   });
 }
+
+// Function for MineCraft Time
+function getTimeFn(request, response) {
+  readFile('data.txt', request, function(err, data) {
+    if (err) {
+      console.log("Recheck Request Please")
+      response()
+    }
+    else {
+      bucksData = JSON.parse(data);
+      response(data);
+    }
+  })
+}
+
+
+
+mineCraftRoute.get("/time", function(req, res) {
+  readFile('data.txt', req, function(err, data) {
+    if (err) {
+      console.log("Recheck Request Please")
+      res.json({"message": "Recheck Request Please"})
+    }
+    else {
+      bucksData = JSON.parse(data);
+      res.json({"user": data})
+    }
+  })
+})
+
+mineCraftRoute.put("/time", function(req, res) {
+  var id = req.params._id;
+  var user = req.body;
+  if (user._id == id) {
+    console.log("Wrong ID");
+    return res.status(401).json({err: "Wrong ID"})
+  }
+
+  //OLD
+  // User.findByIdAndUpdate(id, user, {new: true}, function(err, user) {
+  //   if (err) {
+  //       return res.status(500).json({"err": err.message, 'message':'Profile Failed Updated'});
+  //   }
+  //   res.send({'user': user, 'message':'Profile Updated'});
+  // })
+})
+
+// Mine Craft Route to Call
+app.use('/api/mc', mineCraftRoute);
 
 // Finally, initialize the server on any port, and go to that url
 // in your browser
